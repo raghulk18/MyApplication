cf0a79466090c52fce8469c581938ecd945ca96e
Kumaran, Raghul <raghul.kumaran@accenture.com>
2021-10-05 11:07:41 +0530

    cafe folder structures

---
+++ src/cafe/cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./cart.scss"
+
+function Cart() {
+    return ( 
+        <div className="cart-container">
+            cart
+        </div>
+     );
+}
+
+export default Cart;
