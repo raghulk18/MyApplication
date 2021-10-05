5d0afe0da38e1e845250ea4c6326928924e1dcd1
nagarajan.sethu <nagarajan.sethu@accenture.com>
2021-10-05 20:44:50 +0530

    contact us page is added

--- src/cafe/components/navbar/navbar.jsx
+++ src/cafe/components/navbar/navbar.jsx
@@ -1,14 +1,12 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./navbar.scss";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 const logoUrl = window.location.origin + "/images/logo.svg"; 
-const navbarAPiUrl = "/navbar.json"
+const navbarAPiUrl = "/json/navbar.json"
 
 function Navbar(props) {
 
     const [navbarData, setnavbarData] = useState();
-    // const [linkData, setlinkData] = useState(props);
     
     const getData=()=>{
         fetch(navbarAPiUrl)
@@ -26,14 +24,15 @@ function Navbar(props) {
         <div className="navbar-left-container">
             <div className="logo">
                 <a href="/" className="logo-container" aria-label="home">
-                    <img src={logoUrl}/>
+                    <img src={logoUrl} alt={'logoUrl'}/>
                 </a>
             </div>
      
             <div className="navigation-menu">
                 { navbarData && navbarData.map(nav => (
-                    <a className="menu-link" key={nav.id} href={nav.path}>{nav.text}</a>
-                ))}
+                    <a className="menu-link" key={nav.id} href={nav.url}>{nav.text}</a>
+                ))
+                }
 
             </div>
         </div>
